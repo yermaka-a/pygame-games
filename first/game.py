da9799493b4cb525e79301d72456a75f1ac9c59d
yermaka <ermak.080@mail.ru>
2025-08-25 07:44:37 +0800

    add Tilemap

--- first/game.py
+++ first/game.py
@@ -4,7 +4,7 @@ import pathlib
 from pathlib import Path
 from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity 
-
+from scripts.tilemap import Tilemap
 ROOT = pathlib.Path.cwd()
 
 print(ROOT)
@@ -15,6 +15,9 @@ class Game:
         pygame.display.set_caption('ninja game')
         self.screen = pygame.display.set_mode((640, 480))
         self.clock = pygame.time.Clock()
+        self.display = pygame.Surface((320, 240))
+        self.movement = [False, False]
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
         
         self.assets = {
             'decor': load_images('tiles/decor'), 
@@ -23,16 +26,16 @@ class Game:
             'stone': load_images('tiles/stone'), 
             'player': load_image('entities/player.png')
         }
-        self.display = pygame.Surface((320, 240))
-        self.movement = [False, False]
-        self.collision_area = pygame.Rect(50, 50, 300, 50)
+        
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+        
+        self.tilemap = Tilemap(self, tile_size=16)
     def run(self): 
         while True:
             self.display.fill((14, 219, 248))
             self.player.update((self.movement[1] - self.movement[0], 0))            
             self.player.render(self.display)
-            
+            self.tilemap.render(self.display)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
