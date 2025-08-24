43ba94e7cf4b4eecee1319dff3db33181476b257
yermaka <ermak.080@mail.ru>
2025-08-25 07:29:49 +0800

    upload assets

--- first/game.py
+++ first/game.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 import pathlib
 from pathlib import Path
-from scripts.utils import load_image
+from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity 
 
 ROOT = pathlib.Path.cwd()
@@ -17,17 +17,21 @@ class Game:
         self.clock = pygame.time.Clock()
         
         self.assets = {
+            'decor': load_images('tiles/decor'), 
+            'grass': load_images('tiles/grass'), 
+            'large_decor': load_images('tiles/large_decor'), 
+            'stone': load_images('tiles/stone'), 
             'player': load_image('entities/player.png')
         }
-        
+        self.display = pygame.Surface((320, 240))
         self.movement = [False, False]
         self.collision_area = pygame.Rect(50, 50, 300, 50)
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
     def run(self): 
         while True:
-            self.screen.fill((14, 219, 248))
+            self.display.fill((14, 219, 248))
             self.player.update((self.movement[1] - self.movement[0], 0))            
-            self.player.render(self.screen)
+            self.player.render(self.display)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -39,20 +43,15 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
-                    if event.key == pygame.K_UP:
-                        self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
+                 
                         self.movement[1] = True
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-                    if event.key == pygame.K_UP:
-                        self.movement[0] = False
-                    if event.key == pygame.K_DOWN:
-                        self.movement[1] = False
-                    
+                  
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))        
             pygame.display.update()
             self.clock.tick(60)
             
--- first/scripts/utils.py
+++ first/scripts/utils.py
@@ -1,9 +1,18 @@
 import pygame
 import pathlib
 from pathlib import Path
+import os
+
+
 ROOT = pathlib.Path.cwd() 
 BASE_IMAGE_PATH = 'data/images'
 def load_image(path):
     img = pygame.image.load(Path(ROOT,BASE_IMAGE_PATH, path))
     img.set_colorkey((0, 0, 0))
     return img
+
+def load_images(path):
+    images = []
+    for img_name in os.listdir(Path(BASE_IMAGE_PATH, path)):
+        images.append(load_image(Path(path, img_name)))
+    return images
